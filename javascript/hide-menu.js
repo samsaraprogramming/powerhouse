44ae27cd1d620dc3247c2246087b7a9a51b24682
samsaraprogramming <59861427+samsaraprogramming@users.noreply.github.com>
2023-07-12 20:58:23 -0500

    changing function

--- javascript/hide-menu.js
+++ javascript/hide-menu.js
@@ -7,10 +7,11 @@ var prevScroll = 0;
      // scroll functions
      $(window).scroll(function(e) {
      
+          scroll = $(window).scrollTop()
          // add/remove class to navbar when scrolling to hide/show
-         $('.navbar')[(prevScroll - $(window).scrollTop() <= 10 && $(window).scrollTop() >= 50) ? 'addClass' : 'removeClass']('navbar-hide');
+         $('.navbar')[(prevScroll - scroll <= 10 && $(window).scrollTop() >= 50) ? 'addClass' : 'removeClass']('navbar-hide');
 
-         prevScroll = $(window).scrollTop();
+         prevScroll = scroll;
      
      });
      
